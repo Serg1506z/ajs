225a560b5f634a1ddbd964fc93d44dead30826bd
Serg1506z <136631034+Serg1506z@users.noreply.github.com>
2024-06-01 13:04:20 +0300

    fixed

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/main.js',
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'), // каталог для результатов сборки 
-    filename: 'main.js', // имя файла с результатами сборки (должно совпадать с entry point в шаге 1)
+    filename: 'index.js', // имя файла с результатами сборки (должно совпадать с entry point в шаге 1)
     library: 'ajs', // название нашей библиотеки
     libraryTarget: 'umd', // UMD (Universal Module Definition https://github.com/umdjs/umd) - шаблон, который позволяет использовать RequireJS и браузер
     libraryExport: 'default', // экспортируется имя default
